948cabf51939ceaa6732553c820812a880ad6025
djw-easy <1303886435@qq.com>
2023-04-04 17:15:04 +0800

    add an example

--- geodetector.py
+++ geodetector.py
@@ -4,7 +4,7 @@ from scipy.stats import f
 def factor_dector(df, y: str, factors: list, out_index_name :str ="q-value"):
     out_df = pd.DataFrame(index=[out_index_name], columns=factors, dtype="float32")
     for factor in factors:
-        ssw = df[y, factor].groupby(factor).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
+        ssw = df[[y, factor]].groupby(factor).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
         sst = (df[y].size*df[y].var(ddof=0))
         q = 1-ssw/sst
         out_df.loc[out_index_name, factor] = q
@@ -15,7 +15,8 @@ def interaction_detector(df, y, factors):
     length = len(factors)
     for i in range(0, length):
         for j in range(0, i+1):
-            ssw = df[y, factors[i], factors[j]].groupby([factors[i], factors[j]]).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
+            ssw = df[[y]+list(set([factors[i], factors[j]]))].groupby([factors[i], factors[j]]).apply(
+                lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
             sst = (df[y].size*df[y].var(ddof=0))
             q = 1-ssw/sst
             out_df.loc[factors[i], factors[j]] = q
@@ -25,10 +26,10 @@ def ecological_detector(df, y, factors):
     out_df = pd.DataFrame(index=factors, columns=factors, dtype="float32")
     length = len(factors)
     for i in range(1, length):
-        ssw1 = df[y, factors[j]].groupby(factors[i]).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
+        ssw1 = df[[y, factors[i]]].groupby(factors[i]).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
         dfn = df[factors[i]].notna().sum()-1
         for j in range(0, i):
-            ssw2 = df[y, factors[j]].groupby(factors[j]).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
+            ssw2 = df[[y, factors[j]]].groupby(factors[j]).apply(lambda x:x.shape[0]*x.var(ddof=0))[y].sum()
             dfd = df[factors[j]].notna().sum()-1
             fval = (dfn*(dfd-1)*ssw1)/(dfd*(dfn-1)*ssw2)
             if fval<f.ppf(0.05, dfn, dfn):
